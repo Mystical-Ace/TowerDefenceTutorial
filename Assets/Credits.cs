11a5f71c42bf191561c4feeba431609152339ccd
Mystical-Ace <dm.oakley@gmail.com>
2024-11-11 05:06:40 -0700

    Added Turret art and added scripts to allow for turret rotation

---
+++ Assets/Credits.cs
@@ -0,0 +1,10 @@
+/* 
+
+This is a credits only page. No scripts are in effect. A C# script file is the easiest way to keep credit notes within the project file and easily accessed.
+
+
+Game Tutorial by Muddy Wolf: https://youtube.com/playlist?list=PLfX6C2dxVyLz_w9AWxvkRKc2zUvBl0GIl&si=Mc9-VkSe_as3Rup9
+
+"Tower Defence Towers" by Nido licensed CC0: https://opengameart.org/node/114170
+
+*/
